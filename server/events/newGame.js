6bb214ed53074e8b0712150fcaa0adccaefbdc01
= <herlon214@gmail.com>
2018-05-29 01:27:39 -0400

    Correção de matchId ao entrar no jogo

--- server/events/newGame.js
+++ server/events/newGame.js
@@ -1,4 +1,5 @@
 // Libs
+const debug = require('debug')('truco-royale:events')
 const Game = require('../game')
 const GenerateId = require('generate-id')
 const guid = new GenerateId()
@@ -6,6 +7,8 @@ const guid = new GenerateId()
 module.exports = (context, socket, data, callback) => {
   const id = guid.generate({ length: 3, include: ['numbers'] })
   const game = new Game(id)
+
+  debug(`Game ${id} created!`)
 
   context.games.push(game)
 
